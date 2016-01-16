042c52e8adc6e7274ff800981995245edff32676
Philippe Boessling <phib@phib.de>
2016-01-16 16:46:26 +0100

    Added ListView with checkboxes and hardcoded task strings.

--- src/main/java/de/phib/tasket/Main.java
+++ src/main/java/de/phib/tasket/Main.java
@@ -1,8 +1,12 @@
 package de.phib.tasket;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,15 +18,38 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Label label = new Label("Hello World!");
+		ObservableList<String> tasks = FXCollections.observableArrayList();
+		tasks.addAll("Task 1", "Task 2", "Task 3");
+
+		ListView<String> tasksListView = new ListView<>();
+		tasksListView.setCellFactory((param) -> {
+			return new TaskCell();
+		});
+		tasksListView.setItems(tasks);
 
 		StackPane root = new StackPane();
-		root.getChildren().add(label);
+		root.getChildren().add(tasksListView);
 
 		Scene scene = new Scene(root);
 
 		primaryStage.setTitle("Tasket");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	private static class TaskCell extends ListCell<String> {
+
+		private CheckBox checkbox = new CheckBox();
+
+		@Override
+		protected void updateItem(String item, boolean empty) {
+			super.updateItem(item, empty);
+
+			if (item != null && !empty) {
+				setText(item);
+				setGraphic(checkbox);
+			}
+		}
+
 	}
 }
