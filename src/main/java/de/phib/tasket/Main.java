165d26d7f1a15aaf17862e75520e73fee76011ec
Philippe Boessling <phib@phib.de>
2016-02-07 16:51:38 +0100

    Added FXML file and controller.

--- src/main/java/de/phib/tasket/Main.java
+++ src/main/java/de/phib/tasket/Main.java
@@ -1,14 +1,9 @@
 package de.phib.tasket;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ToolBar;
-import javafx.scene.control.cell.TextFieldListCell;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -19,20 +14,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		ToolBar toolBar = new ToolBar(new Button("Add"), new Button("Delete"), new Button("Move up"),
-				new Button("Move down"), new Button("Mark as done"));
-
-		ObservableList<String> tasks = FXCollections.observableArrayList();
-		tasks.addAll("Task 1", "Task 2", "Task 3");
-
-		ListView<String> tasksListView = new ListView<>();
-		tasksListView.setCellFactory(TextFieldListCell.forListView());
-		tasksListView.setEditable(true);
-		tasksListView.setItems(tasks);
-
-		BorderPane root = new BorderPane();
-		root.setTop(toolBar);
-		root.setCenter(tasksListView);
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/Tasket.fxml"));
 
 		Scene scene = new Scene(root);
 
---
+++ src/main/java/de/phib/tasket/MainController.java
@@ -0,0 +1,48 @@
+package de.phib.tasket;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.TextFieldListCell;
+
+public class MainController implements Initializable {
+
+	@FXML
+	private Button addButton;
+
+	@FXML
+	private Button deleteButton;
+
+	@FXML
+	private Button moveUpButton;
+
+	@FXML
+	private Button moveDownButton;
+
+	@FXML
+	private Button markAsDoneButton;
+
+	@FXML
+	private ListView<String> tasksListView;
+
+	private ObservableList<String> tasks = FXCollections.observableArrayList();
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		tasksListView.setCellFactory(TextFieldListCell.forListView());
+		tasksListView.setEditable(true);
+		tasksListView.setItems(tasks);
+	}
+
+	@FXML
+	public void addTask() {
+		tasks.add(new String("New Task"));
+	}
+
+}
